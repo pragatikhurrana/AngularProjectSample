e5be523e2629b8e1b4374d3ce6ae3f0ad2d20590
pragatikhurana <pragatikhurana0@gmail.com>
2026-09-04 23:40:44 +0530

    signal getters setters computed

--- AngularProject/src/app/user/user.component.ts
+++ AngularProject/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,computed,signal } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -16,10 +16,17 @@ export class UserComponent {
     {name: 'Minu Khurana', avatar: 'user-icon.png'}
   ];
 
-  selectedUser = this.dummyUser[0];
-  get imagePath(){
-    return 'assets/' + this.selectedUser.avatar;
-  }
+  //selectedUser = this.dummyUser[0];
+    selectedUser = signal(this.dummyUser[0]);
+//signal is a new feature in Angular that allows you to create reactive state in your components.
+//It is similar to the useState hook in React. 
+//The signal function takes an initial value and returns a signal object that can be used to read and update the state. 
+// In this case, the selectedUser signal is initialized with the first user in the dummyUser array.
+  imagePath = computed(() => 'assets/' + this.selectedUser().avatar);
+//   get imagePath(){
+//     return 'assets/' + this.selectedUser().avatar;
+//     //this.selectedUser().avatar; getter is used to compute the value of the imagePath property based on the selectedUser signal.
+//   }
 
 //dynamic data binding is used to bind the selectedUser property to the template. The selectedUser property is an object that contains the name and avatar of the user, and it is used to display the user's name and avatar in the template. 
 // The selectedUser property is initialized with a default value of 'Pragati Khurana' and 'user-icon.png', which will be displayed in the template when the component is rendered.
@@ -27,7 +34,10 @@ export class UserComponent {
 //property binding is used to bind the image source to the src attribute of the img element. The value of the src attribute is set to 'assets/user-icon.png', which is the path to the user icon image in the assets folder.
 onSelectUser(){
   const randomIndex = Math.floor(Math.random() *this.dummyUser.length);
-  this.selectedUser = this.dummyUser[randomIndex];
+  this.selectedUser.set(this.dummyUser[randomIndex]);
+  //set can be used to update the value of a signal. 
+  // In this case, the selectedUser signal is updated with a random user from the dummyUser array when the onSelectUser() method is called.
+  //this.selectedUser = this.dummyUser[randomIndex];
   console.log('User selected:', this.selectedUser);
 }
 }
