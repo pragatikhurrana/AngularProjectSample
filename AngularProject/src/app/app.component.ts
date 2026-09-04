cd8c2fdc0ab04383feb6ded1cfa4cd9041491033
pragatikhurana <pragatikhurana0@gmail.com>
2026-09-05 00:57:30 +0530

    tasks

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,7 +21,13 @@ export class AppComponent {
     { id: 5, name: 'Minu Khurana', avatar: 'user-icon.png'}
   ];
   title = 'AngularProject';
+  selectedUserId: number = 1;
   onSelectUser(id: number) {
     console.log('User selected:', id);
+    this.selectedUserId = id;
+  }
+
+  getSelectedUser(){
+    return this.users.find(user => user.id === this.selectedUserId);
   }
 }
